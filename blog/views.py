db349c554fed91e494c3a286d10bce89255db9dd
ttran9 <toddtran9@gmail.com>
2018-11-26 14:17:15 -0800

    removing junk data and grabbing posts from the database.

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2018'
-    },
-]
+from .models import Post
 
 
 # Create your views here.
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     # return HttpResponse('<h1>Blog Home</h1>')
     return render(request, 'blog/home.html', context)

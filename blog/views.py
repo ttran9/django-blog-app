1abf572104fbaa9372a642b3817c9153e380a7a1
ttran9 <toddtran9@gmail.com>
2018-11-26 22:40:01 -0800

    Creating a custom class based view to display posts for a user.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
 from django.views.generic import (
     ListView,
     DetailView,
@@ -24,6 +25,20 @@ class PostListView(ListView):
     template_name = 'blog/home.html' # <app>/<model>_<viewtype>.html
     context_object_name = 'posts'
     ordering = ['-date_posted'] # take away "-" to make it go in ascending order.
+    paginate_by = 5 # number of objects/posts per page.
+
+
+class UserPostListView(ListView):
+    model = Post # the model to be queried.
+    template_name = 'blog/user_posts.html' # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    paginate_by = 5 # number of objects/posts per page.
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
+
+
 
 
 class PostDetailView(DetailView):

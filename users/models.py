603f128205cd56694be3dd338282ffc8cfee8594
ttran9 <toddtran9@gmail.com>
2018-11-26 18:50:11 -0800

    Defining the model for the Profile object.

--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Profile(models.Model):
+    # enforce a user to have a one to one relationship with a profile.
+    # if we delete the user delete the profile but not necessarily the inverse.
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'

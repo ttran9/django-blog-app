e1006540b7e6429760e1d709f85621cc07a7d2aa
ttran9 <toddtran9@gmail.com>
2018-11-26 19:41:15 -0800

    Adding in a form where the user can update the user name and email and where the user can modify their image/avatar.

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 
 class UserRegisterForm(UserCreationForm):
@@ -12,3 +13,20 @@ class UserRegisterForm(UserCreationForm):
         model = User
         # fields to be shown on the form in a specified order.
         fields = ['username' ,'email', 'password1', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        # the model that we want this form to interact with.
+        # when this form validates it creates a user.
+        model = User
+        # fields to be shown on the form in a specified order.
+        fields = ['username', 'email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']

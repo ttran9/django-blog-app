62c27f27ca2cf94879a4099d5968ddb6416b4422
ttran9 <toddtran9@gmail.com>
2018-11-26 18:53:48 -0800

    Connecting the signals with the User object.

--- users/apps.py
+++ users/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class UsersConfig(AppConfig):
     name = 'users'
+
+    def ready(self):
+        # https://docs.djangoproject.com/en/2.1/topics/signals/ (for explanation for this function).
+        import users.signals
